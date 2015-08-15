7b840eeb4e1ca5e4d9ad9fe9c4ff79c5fffa793b
Ron Cordell <ron.cordell@berkeley.edu>
2015-08-14 23:26:27 -0700

    mongodb data extraction program to move the data out of MongoDB to S3

--- project/dataChecks/mongoExtract.py
+++ project/dataChecks/mongoExtract.py
@@ -5,10 +5,10 @@ mongoClient = MongoClient('ec2-54-153-43-230.us-west-1.compute.amazonaws.com')
 db = mongoClient.twitter_db
 
 i = 0
-with open('ebola.json','w') as isis:
-	documents = db['ebola'].find()
+with open('trump.json','w') as f:
+	documents = db['trump'].find()
 	for doc in documents:
-		isis.write(dumps(doc))
+		f.write(dumps(doc))
 		i += 1
 		if i % 1000 == 0:
 			print '{0} docs written...'.format(i)
