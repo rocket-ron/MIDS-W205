46b575d52084e938251be520e267acf5354b6d71
Ron Cordell <ron.cordell@berkeley.edu>
2015-08-15 21:51:51 -0700

    fear_count.py now multi-step to include a sorting step. fear_timeline now working with mrjob

--- project/sentiment_analysis/fear_timeseries.py
+++ project/sentiment_analysis/fear_timeseries.py
@@ -7,15 +7,33 @@ Created on Mon Jul  6 19:03:10 2015
 """
 
 import pickle
+import re
 
 from mrjob.job import MRJob
 from datetime import datetime
+from mrjob.protocol import JSONProtocol, JSONValueProtocol
+
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+conn = S3Connection(host="s3-us-west-1.amazonaws.com")
+bucket = conn.get_bucket('w205-rcordell-project')
+f_key = bucket.get_key('emr/classifier.pkl')
+v_key = bucket.get_key('emr/vectorizer.pkl')
+tlc = pickle.loads(f_key.get_contents_as_string())
+vec = pickle.loads(v_key.get_contents_as_string())
+
+"""
+
+When reading the pickle files from a local store use the
+code below.
+
 
 with open("classifier.pkl","rb") as f:
     tlc=pickle.load(f)
 with open("vectorizer.pkl","rb") as v:
     vec=pickle.load(v)
-    
+"""
+
 def predict_one (predict_tweet,trained_classifier, vectorizer):
     predict_data=[predict_tweet]
     predict_vectors = vectorizer.transform(predict_data)
@@ -24,14 +42,17 @@ def predict_one (predict_tweet,trained_classifier, vectorizer):
         
 class MRFearTweetCount(MRJob):
 
-    def mapper(self, _, line):
-        if line.find('<split>')>0:
-            tweet=line.split("<split>")
-            text=tweet[0]
-            user=tweet[1]
-            date=datetime.strptime(tweet[3],"%c").strftime("%x")
-            emotion=predict_one(text,tlc,vec)
-            if emotion=='fear':
+    INPUT_PROTOCOL = JSONValueProtocol
+
+    def mapper(self, _, tweet):
+        self.increment_counter('group_1', 'total_tweets', 1)
+        if 'user' in tweet and 'text' in tweet and 'created_at' in tweet:
+            text = tweet['text']
+            user = tweet['user']['id_str']
+            date_str = (tweet['created_at']).replace("+0000 ","")
+            date = datetime.strptime(date_str,"%a %b %d %H:%M:%S %Y").strftime("%x")
+            emotion = predict_one(text, tlc, vec)
+            if emotion == 'fear':
                 yield date,1
             else:
                 yield date,0
@@ -51,4 +72,6 @@ if __name__ == '__main__':
 #Simple version: ./fear_count.py test.csv
 
 #To run on EMR:
-#python fear_count.py -r emr --conf-path mrjob.conf s3://hamlin-mids-mapreduce/input/test.csv
+#python fear_timeseries.py -r emr --conf-path mrjob.conf s3://hamlin-mids-mapreduce/input/test.csv
+
+#python fear_timeseries.py -r emr --no-output --output-file s3://w205-rcordell-project/emr/fearcounts --conf-path mrjob.conf s3://w205-rcordell-project/emr/data/ebola.json
